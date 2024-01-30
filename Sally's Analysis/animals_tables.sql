daa4432ce1187e68d14dcc2775c998225aaef913
sallyca530 <sally.ca530@gmail.com>
2024-01-29 21:06:09 -0800

    troubleshooting

---
+++ Sally's Analysis/animals_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE animals (
+    column1 INTEGER,
+    id NUMERIC,
+    organization VARCHAR(255),
+	state VARCHAR(255),
+    name VARCHAR(255),
+    type VARCHAR(255),
+    age VARCHAR(255),
+    gender VARCHAR(255),
+    size VARCHAR(255),
+	url VARCHAR(500)
+);
+
+Select * From animals;
